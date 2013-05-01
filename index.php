178493f92e15f1709663d0d21426077f26b9b1b9
Rodolfo Ferreira de Oliveira <oliveira.rodolfo@prox3.com.br>
2013-05-01 14:57:55 -0300

    Menu e Footer

--- index.php
+++ index.php
@@ -7,20 +7,43 @@
 <title>PROX3</title>
 <link rel="stylesheet" type="text/css" href="css/css.css"/>
 <link rel="stylesheet" type="text/css" href="css/media.css"/>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/index.js"></script>
 </head>
 <body>
 <div id="tudo">
     <div id="header">
         <div id="logo">
-        	<img src="images/logo.png" alt="PROX3" width="100%" height="100%" />
+        	<a href="index.php" target="_self">
+            	<img src="images/logo.png" alt="PROX3" width="100%" height="100%" />
+            </a>
         </div>
         <div>
         	<h1>Produtora Digital</h1>
 		</div>
     </div>
     <div id="content">
+    	<center>
+            <dl class="accordion">
+                <dt><a href="">o que fazemos</a></dt>
+                <dd>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</dd>
+                
+                <dt><a href="">clientes</a></dt>
+                <dd>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</dd>
+            
+                <dt><a href="">trabalhe com a gente</a></dt>
+                <dd>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</dd>
+                
+                <dt><a href="">contatos</a></dt>
+                <dd>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</dd>
+            
+            </dl>
+        </center>
     </div>
     <div id="footer">
+    	<a href="https://www.facebook.com/prox3.digital" target="_blank">
+        	<img src="images/facebook.png" alt="Facebook - Prox3" width="100%" height="100%" />
+		</a>
     </div>
 </div>
 </body>
