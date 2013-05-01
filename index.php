52ef1210566e3ee0e9f224f8a63b192eb4c05b54
Rodolfo Ferreira de Oliveira <oliveira.rodolfo@prox3.com.br>
2013-05-01 17:21:56 -0300

    Site Mobile

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width,user-scalable=no">
-<meta name="viewport" content="width=device-width, minimum-scale=1,maximum-scale=1">
-<title>PROX3</title>
-<link rel="stylesheet" type="text/css" href="css/css.css"/>
-<link rel="stylesheet" type="text/css" href="css/media.css"/>
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/index.js"></script>
+	<title>Prox3 - Produtora Digital</title>
+    <meta name="description" content="Desenvolva seus projetos online com a Prox3 de forma rápida, criativa e de qualidade.">
+    <meta name="keywords" content="prox3, produtora, produtora digital, sites, agência digital">
+    <meta name="organization name" content="Prox3">
+    <meta http-equiv="content-language" content="pt-br">
+    <meta name="language" content="portuguese">
+    <meta name="location" content="Brazil">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="expires" content="-1">
+    <meta name="mssmarttagspreventparsing" content="true">
+    <meta http-equiv="imagetoolbar" content="no">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width,user-scalable=no">
+    <meta name="viewport" content="width=device-width, minimum-scale=1,maximum-scale=1">
+    <title>Prox3</title>
+    <link rel="stylesheet" type="text/css" href="css/css.css"/>
+    <link rel="stylesheet" type="text/css" href="css/media.css"/>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/index.js"></script>
 </head>
 <body>
 <div id="tudo">
@@ -25,18 +36,119 @@
     <div id="content">
     	<center>
             <dl class="accordion">
-                <dt><a href="">o que fazemos</a></dt>
-                <dd>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</dd>
+                <dt><span>como pensamos</span></dt>
+                <dd class="active">
+                	<div class="boxTextos">
+                        <p>Mergulhar no universo de nossos parceiros, usar todo nosso know-how e criatividade para realizar projetos singulares e experiências marcantes.
+                        <br/><br/>
+                        <strong>Nós somos a Prox3 e, aqui, a sua marca é levada a sério.</strong>
+                        </p>
+                    </div>
+                </dd>
                 
-                <dt><a href="">clientes</a></dt>
-                <dd>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</dd>
-            
-                <dt><a href="">trabalhe com a gente</a></dt>
-                <dd>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</dd>
+                <dt><span>o que fazemos</span></dt>
+                <dd>
+                	<div class="boxTextos">
+                        <h2>SITES</h2>
+                        <p>
+                        A imagem virtual de uma empresa, nos dias de hoje, pode ser um fator vital para o fechamento de um grande negócio. 
+                        Cuide bem de sua aparência. Na Prox3, você encontra sites institucionais, e-commerce, atualização e manutenção periódica.
+                        </p>
+                    </div>
+                    <div class="boxTextos">
+                    	<h2>SISTEMAS</h2>
+                        <p>
+                        Otimize o seu tempo com os sistemas da Prox3. Com eles, a troca de informações entre colaboradores, fornecedores e clientes fica mais ágil e simples. 
+                        </p>
+                    </div>
+                    <div class="boxTextos">
+                        <h2>APLICATIVOS FACEBOOK</h2>
+                        <p>
+                        Sua marca muito bem vista nas redes sociais. É isso que você quer. É isso que a Prox3 quer. Cuidamos da sua presença, neste ambiente, através do desenvolvimento de promoções, vendas personalizadas e outras ferramentas. Conte com a gente.
+                        </p>
+                    </div>
+                    <div class="boxTextos">
+                        <h2>CONSULTORIA</h2>
+                        <p>
+                        Consultoria personalizada em informática, projetos e e-commerce. 
+                        </p>
+                    </div>
+                    <div class="boxTextos">
+                        <h2>APLICATIVO MOBILE</h2>
+                        <p>
+                        Os smartphones estão em alta e o seu uso no ambiente corporativo se tornou uma constante. Através da criação de aplicativos e da adaptação de sites/blogs para estes aparelhos, a Prox3 leva a sua marca para o seu público, onde quer que ele esteja. 
+                        </p>
+                    </div>
+                </dd>
                 
-                <dt><a href="">contatos</a></dt>
-                <dd>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.</dd>
+                <dt><span>clientes</span></dt>
+                <dd>
+                	<div class="boxTextos">
+                    	<div class="boxClientes">
+                        	<a href="#" target="_blank">
+                            	<img src="images/logo.png" width="100%" height="100%" />
+                            </a>
+                        </div>
+                        <div class="boxClientes">
+                        	<a href="#" target="_blank">
+                            	<img src="images/logo.png" width="100%" height="100%" />
+                            </a>
+                        </div>
+                        <div class="boxClientes">
+                        	<a href="#" target="_blank">
+                            	<img src="images/logo.png" width="100%" height="100%" />
+                            </a>
+                        </div>
+                        
+                        <div class="boxClientes">
+                        	<a href="#" target="_blank">
+                            	<img src="images/logo.png" width="100%" height="100%" />
+                            </a>
+                        </div>
+                        <div class="boxClientes">
+                        	<a href="#" target="_blank">
+                            	<img src="images/logo.png" width="100%" height="100%" />
+                            </a>
+                        </div>
+                        <div class="boxClientes">
+                        	<a href="#" target="_blank">
+                            	<img src="images/logo.png" width="100%" height="100%" />
+                            </a>
+                        </div>
+                        
+                        <div class="boxClientes">
+                        	<a href="#" target="_blank">
+                            	<img src="images/logo.png" width="100%" height="100%" />
+                            </a>
+                        </div>
+                        <div class="boxClientes">
+                        	<a href="#" target="_blank">
+                            	<img src="images/logo.png" width="100%" height="100%" />
+                            </a>
+                        </div>
+                        <div class="boxClientes">
+                        	<a href="#" target="_blank">
+                            	<img src="images/logo.png" width="100%" height="100%" />
+                            </a>
+                        </div>
+                    </div>
+                </dd>
             
+                <dt><span>portfolio</span></dt>
+                <dd>
+                	<div class="boxTextos">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
+                        </p>
+                    </div>
+                </dd>
+                
+                <dt><span>contatos</span></dt>
+                <dd>
+                	<div class="boxTextos">
+                        <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
+                        </p>
+                    </div>
+                </dd>         
             </dl>
         </center>
     </div>
--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
 	var allPanels = $('.accordion > dd').hide();
-	$('.accordion > dt > a').click(function() {
-	  $this = $(this);
-	  $target =  $this.parent().next();
 	
-	  if(!$target.hasClass('active')){
-		 allPanels.removeClass('active').slideUp();
-		 $target.addClass('active').slideDown();
-	  }
-	return false;
+	$('.accordion > dt:first > span').addClass('active');
+	$('.accordion > dd:first').addClass('active').slideDown();
+	
+	$('.accordion > dt > span').click(function() {
+	  	$this = $(this);
+	  	$target =  $this.parent().next();
+		
+		$('.accordion > dt > span').removeClass('active');
+		$this.addClass('active');
+		
+	  	if(!$target.hasClass('active')){
+			 allPanels.removeClass('active').slideUp();
+			 $target.addClass('active').slideDown();
+	  	}
+		return false;
 	});
 });
