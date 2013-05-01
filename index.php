4ab3f07239a71f784577c82ad8825238fc765b59
Rodolfo Ferreira de Oliveira <oliveira.rodolfo@prox3.com.br>
2013-05-01 13:38:56 -0300

    Index - Mobile

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width,user-scalable=no">
+<meta name="viewport" content="width=device-width, minimum-scale=1,maximum-scale=1">
+<title>PROX3</title>
+<link rel="stylesheet" type="text/css" href="css/css.css"/>
+<link rel="stylesheet" type="text/css" href="css/media.css"/>
+</head>
+<body>
+<div id="tudo">
+    <div id="header">
+        <div id="logo">
+        	<img src="images/logo.png" alt="PROX3" width="100%" height="100%" />
+        </div>
+        <div>
+        	<h1>Produtora Digital</h1>
+		</div>
+    </div>
+    <div id="content">
+    </div>
+    <div id="footer">
+    </div>
+</div>
+</body>
+</html>
