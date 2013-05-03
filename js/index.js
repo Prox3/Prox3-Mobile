608391599c4cbe218a9dbff00f9bf465a8d99e1f
Rodolfo Ferreira de Oliveira <oliveira.rodolfo@prox3.com.br>
2013-05-02 23:21:01 -0300

    Add Analytics

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
+var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-38821032-1']);
+	_gaq.push(['_trackPageview']);
+
+	(function () {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+
 Zepto(document).ready(function(){
+	setTimeout(function(){
+		window.scrollTo(0, 1);
+	}, 0);
 	var $ = Zepto;
-	var allPanels = $('.accordion > dd').hide();
-	
-	//$('.accordion > dt:first > span').addClass('active');
-	//$('.accordion > dd:first').addClass('active').slideDown();
-	
+	var allPanels = $('.accordion > dd').hide();	
 	$('.accordion > dt > span').click(function() {
 	  	$this = $(this);
 	  	$target =  $this.parent().next();
@@ -18,10 +27,4 @@ Zepto(document).ready(function(){
 	  	}
 		return false;
 	});
-//	var deviceAgent = navigator.userAgent.toLowerCase();
-//    var agentID = deviceAgent.match(/(iphone|ipod|android)/);
-//
-//    if (!agentID) {
-//		$("#lnkFace").attr("href","https://www.facebook.com/prox3.digital");
-//	}
 });
